ca822de1a86f757db1fd76e6d57ee487b0650afd
Daniel McCoshen <upstagecoronet@gmail.com>
2017-03-22 17:12:47 -0600

    some more changes

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -4,6 +4,8 @@ import model.CodeType;
 import model.Tabula;
 
 import javax.swing.*;
+import java.awt.*;
+
 
 /**
  * Created by daniel on 22/03/17.
@@ -20,10 +22,25 @@ public class MainWindow extends JFrame {
 
     private JLabel inputDesc = new JLabel("Enter Message");
     private JLabel codeDesc = new JLabel("Code:");
-    private JLabel tabulaDesc = new JLabel()
+    private JLabel tabulaDesc = new JLabel("Tabula:");
+
+    private JPanel edgePanel = new JPanel();
+    private JPanel botomPanel = new JPanel();
 
     public MainWindow(){
+        setLayout(new BorderLayout());
+        edgePanel.setLayout(new GridLayout(1, 4));
+        edgePanel.add(codeDesc);
+        edgePanel.add(codes);
+        edgePanel.add(tabulaDesc);
+        edgePanel.add(tabulas);
+        add(edgePanel, BorderLayout.EAST);
 
+        botomPanel.add(encode);
+        botomPanel.add(decode);
+        add(botomPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
     }
 
 }
