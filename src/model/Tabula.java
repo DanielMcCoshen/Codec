e645343dab5c6f3b186a24cd6cb47f1f72aca7c2
exalaber <pcunfer@gmail.com>
2017-03-23 10:33:29 -0600

    The first version of the Tabula class. feedback?

--- src/model/Tabula.java
+++ src/model/Tabula.java
@@ -4,4 +4,38 @@ package model;
  * Created by daniel on 22/03/17.
  */
 public class Tabula {
+
+    private String table;
+    private int length;
+
+    public Tabula(String characters){
+        // sets the table to characters, and length to the length of characters
+        table = characters;
+        length = characters.length();
+    }
+
+
+    public char get(char cstart, char coffset){
+        // converts chars to ints
+        int istart = table.indexOf(cstart);
+        int ioffset = table.indexOf(coffset);
+
+        // checks to see if cstart and coffset are in table
+        if ((istart >= 0) && (ioffset >= 0)) {
+
+            // initializes idx and simulates an infinite table by wrapping idx
+            int idx = istart + ioffset;
+            while (idx >= length){
+                idx -= length;
+            }
+            while (idx < 0){
+                idx += length;
+            }
+
+            return table.charAt(idx);
+        } else{
+            // placeholder: a character that should never be in a message or a table
+            return '\n';
+        }
+    }
 }
