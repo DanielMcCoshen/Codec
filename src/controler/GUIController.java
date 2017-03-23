6335764261d83ccc3b150f5e942eb35a27e097d7
Daniel McCoshen <upstagecoronet@gmail.com>
2017-03-22 18:46:59 -0600

    Dan was an idiot and forgot key input. plus other fixes. i hate  swing =(

---
+++ src/controler/GUIController.java
@@ -0,0 +1,7 @@
+package controler;
+
+/**
+ * Created by daniel on 22/03/17.
+ */
+public class GUIController {
+}
--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -15,43 +15,68 @@ public class MainWindow extends JFrame {
     private JButton encode = new JButton("Encode");
     private JButton decode = new JButton("Decode");
 
-    private JTextArea input = new JTextArea("");
+    private JTextArea input = new JTextArea(15,33);
+    private JTextArea key = new JTextArea(15,33);
+    private JScrollPane inputPane = new JScrollPane(input);
+    private JScrollPane keyPane = new JScrollPane(key);
 
     private JComboBox<CodeType> codes = new JComboBox<>();
     private JComboBox<Tabula> tabulas = new JComboBox<>();
 
     private JTextField infile = new JTextField(10);
     private JTextField outfile = new JTextField(10);
+    private JTextField keyfile = new JTextField(10);
 
     private JLabel inputDesc = new JLabel("Message:");
+    private JLabel keyDesc = new JLabel("Key:");
     private JLabel codeDesc = new JLabel("Code:");
     private JLabel tabulaDesc = new JLabel("Tabula:");
+
     private JButton infileDesc = new JButton("Input File (Optional):");
     private JButton outfileDesc = new JButton("Output File (Optional):");
+    private JButton keyFileDesc = new JButton("KeyFile (Optional)");
 
     private JPanel edgePanel = new JPanel();
     private JPanel botomPanel = new JPanel();
+    private JPanel centrePanel = new JPanel();
+    private JPanel leftPanel = new JPanel();
+    private JPanel anotherCentrePanel = new JPanel();
 
     public MainWindow(){
+        key.setLineWrap(true);
+        key.setAutoscrolls(true);
+        input.setLineWrap(true);
+        input.setAutoscrolls(true);
+
         setLayout(new BorderLayout());
-        edgePanel.setLayout(new GridLayout(4, 1));
+        edgePanel.setLayout(new GridLayout(6, 1));
         edgePanel.add(codeDesc);
         edgePanel.add(codes);
         edgePanel.add(tabulaDesc);
         edgePanel.add(tabulas);
+        edgePanel.add(encode);
+        edgePanel.add(decode);
         add(edgePanel, BorderLayout.EAST);
 
         botomPanel.add(infileDesc);
         botomPanel.add(infile);
         botomPanel.add(outfileDesc);
         botomPanel.add(outfile);
-        botomPanel.add(encode);
-        botomPanel.add(decode);
+        botomPanel.add(keyFileDesc);
+        botomPanel.add(keyfile);
         add(botomPanel, BorderLayout.SOUTH);
 
-        add(inputDesc, BorderLayout.NORTH);
-        add(input, BorderLayout.CENTER);
+        centrePanel.setLayout(new BoxLayout(centrePanel,BoxLayout.PAGE_AXIS));
+        centrePanel.add(keyDesc);
+        centrePanel.add(keyPane);
+        anotherCentrePanel.add(centrePanel);
 
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
+        leftPanel.add(inputDesc);
+        leftPanel.add(inputPane);
+        anotherCentrePanel.add(leftPanel);
+
+        add(anotherCentrePanel, BorderLayout.CENTER);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setSize(850,300);
